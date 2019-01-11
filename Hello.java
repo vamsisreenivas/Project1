aac31812eb61eb0c0bc4ceb43e42aac69021a56d
vamsisreenivas <46601291+vamsisreenivas@users.noreply.github.com>
2019-01-11 21:07:50 +0530

    "String args" is added!

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 class Hello
 {
-	public static void main()
+	public static void main(String args[])
 	{
-		System.out.println("Hello World!Iam using GitHub for the first time!")
+		System.out.println("Hello World!Iam using GitHub for the first time!");
 	}
 }
